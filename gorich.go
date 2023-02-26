0662c016b936ea4869cf0adbc47f45eac363d7cb
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2023-02-26 12:05:24 -0600

    Update gorich.go

--- gorich.go
+++ gorich.go
@@ -8,113 +8,103 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"sync"
+	"time"
 
-	"github.com/domainr/tldextract"
-	"github.com/miekg/dns"
-	"github.com/shadowscatcher/shodan"
+	"github.com/domainr/whois"
+	"github.com/likexian/doh-go"
+	"github.com/likexian/simplejson-go"
+	"github.com/projectdiscovery/shodan/v2/pkg/shodan"
+	"github.com/tidwall/gjson"
 )
 
-var dnsServer = "1.1.1.1"
-
-type ipinfoResponse struct {
-	IP          string `json:"ip"`
-	ASN         string `json:"asn"`
-	Org         string `json:"org"`
-	City        string `json:"city"`
-	Region      string `json:"region"`
-	Country     string `json:"country"`
-	Postal      string `json:"postal"`
-	Latitude    string `json:"latitude"`
-	Longitude   string `json:"longitude"`
-	Timezone    string `json:"timezone"`
-	Hostname    string `json:"hostname"`
-	Network     string `json:"network"`
-	Anycast     bool   `json:"anycast"`
-	ThreatLevel int    `json:"threat_level"`
-}
+var (
+	resolver        = "1.1.1.1"
+	ipinfoToken     string
+	hostFile        string
+	shodanAPIKey    string
+	shodanClient    *shodan.Client
+	shodanCache     = make(map[string]shodanResponse)
+	httpClient      = &http.Client{Timeout: 10 * time.Second}
+)
 
 type shodanResponse struct {
-	IP    string   `json:"ip"`
-	Ports []int    `json:"ports"`
-	Vulns []string `json:"vulns"`
+	OpenPorts []int
+	Vulns     []string
 }
 
-type prscanResult struct {
-	Domains       []string
-	IPs           []string
-	OpenPorts     []string
-	Vulns         []string
-	ASN           []string
-	Organization  []string
-	City          []string
-	Region        []string
-	Country       []string
+type ipInfoResponse struct {
+	ASN          string `json:"asn"`
+	Organization string `json:"org"`
+	City         string `json:"city"`
+	Region       string `json:"region"`
+	Country      string `json:"country"`
 }
 
-func prscan(hostfile string, ipinfoToken string) {
-	var domains []string
+func main() {
+	flag.StringVar(&hostFile, "hostfile", "", "A txt file with hosts per newline")
+	flag.StringVar(&ipinfoToken, "ipinfo-token", "", "API token for ipinfo.com")
+	flag.StringVar(&shodanAPIKey, "shodan-apikey", "", "API key for Shodan")
+	flag.Parse()
 
-	file, err := os.Open(hostfile)
+	if hostFile == "" {
+		fmt.Println("[ERROR] missing required flag: --hostfile")
+		os.Exit(1)
+	}
+
+	if ipinfoToken == "" {
+		fmt.Println("[ERROR] missing required flag: --ipinfo-token")
+		os.Exit(1)
+	}
+
+	if shodanAPIKey == "" {
+		fmt.Println("[ERROR] missing required flag: --shodan-apikey")
+		os.Exit(1)
+	}
+
+	shodanClient = shodan.NewClient(nil, shodanAPIKey)
+
+	hosts, err := readHostFile(hostFile)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		domains = append(domains, strings.TrimSpace(scanner.Text()))
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
+		fmt.Printf("[ERROR] failed to read host file: %v\n", err)
 		os.Exit(1)
 	}
 
-	// get unique hosts by converting to set and back
-	uniqueDomains := make(map[string]bool)
-	for _, domain := range domains {
-		uniqueDomains[domain] = true
-	}
-	domains = make([]string, 0, len(uniqueDomains))
-	for domain := range uniqueDomains {
-		domains = append(domains, domain)
+	ips, err := resolveHosts(hosts)
+	if err != nil {
+		fmt.Printf("[ERROR] failed to resolve hosts: %v\n", err)
+		os.Exit(1)
 	}
 
-	ips := make([]string, len(domains))
-	wg := sync.WaitGroup{}
-	wg.Add(len(domains))
-	for i, domain := range domains {
-		go func(i int, domain string) {
-			defer wg.Done()
-			dnsresolver := dns.Client{}
-			dnsresolver.Net = "udp"
-			dnsresolver.Timeout = 5 * time.Second
-			m := dns.Msg{}
-			m.SetQuestion(domain+".", dns.TypeA)
-			r, _, err := dnsresolver.Exchange(&m, dnsServer+":53")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			if len(r.Answer) == 0 {
-				return
-			}
-			ip := r.Answer[0].(*dns.A).A.String()
-			ips[i] = ip
-		}(i, domain)
-	}
-	wg.Wait()
-
-	client := shodan.NewClient(nil, "")
-	result := make([]prscanResult, len(domains))
-
-	for i, ip := range ips {
-		if ip == "" {
-			continue
+	results := make([][]string, len(hosts))
+	for i, host := range hosts {
+		ip := ips[i]
+		openPorts, vulns, err := getShodanInfo(ip)
+		if err != nil {
+			fmt.Printf("[WARN] failed to get Shodan info for %s: %v\n", host, err)
 		}
-		openPorts := make([]string, 0)
-		vulns := make([]string, 0)
-		asn := ""
-		organization := ""
-		city := ""
+		ipInfo, err := getIpInfo(ip)
+		if err != nil {
+			fmt.Printf("[WARN] failed to get IP info for %s: %v\n", host, err)
+		}
+		results[i] = []string{
+			fmt.Sprintf("HOST-%d", i),
+			getRootDomain(host),
+			host,
+			ip,
+			intSliceToString(openPorts),
+			strings.Join(vulns, ","),
+			ipInfo.ASN,
+			ipInfo.Organization,
+			ipInfo.City,
+			ipInfo.Region,
+			ipInfo.Country,
+		}
+	}
+
+	fmt.Println("ID;ROOT;DOMAIN;IP;PORTS;CVE;ASN;ORG;CITY;REGION;COUNTRY")
+	for _, result := range results {
+		fmt.Println(strings.Join(result, ";"))
+	}
+}
+
+func readHostFile(filename string)
